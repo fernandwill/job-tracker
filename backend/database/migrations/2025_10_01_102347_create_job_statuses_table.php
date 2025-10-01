c12f69ea77652ab70f3db5eefbeefa130cdda7e1
Fernand William <fernandwilliam23@gmail.com>
2025-10-01 17:49:32 +0700

    installed dependencies

--- backend/database/migrations/2025_10_01_102347_create_job_statuses_table.php
+++ backend/database/migrations/2025_10_01_102347_create_job_statuses_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('job_statuses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->unsignedInteger('sort_order')->default(0);
         });
     }
 
