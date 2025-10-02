075a995c26034e537657f3203dfe5aeb98c9f05b
Fernand William <156734117+fernandwill@users.noreply.github.com>
2025-10-02 19:08:59 +0900

    Remove upcoming interviews feature

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             JobStatusSeeder::class,
-            InterviewSeeder::class,
         ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\InterviewController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\JobNoteController;
 use App\Http\Controllers\JobStatusController;
@@ -8,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['throttle:api'])->group(static function (): void {
     Route::get('job-statuses', [JobStatusController::class, 'index'])->name('job-statuses.index');
-    Route::get('interviews/upcoming', [InterviewController::class, 'index'])->name('interviews.upcoming');
 
     Route::apiResource('jobs', JobController::class);
 
